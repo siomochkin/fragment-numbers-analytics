2960fe8e1462f33a103659e224ec0d77c244ca71
DenysReryt <denysreryt28@gmail.com>
2023-08-22 16:27:57 +0300

    add parsing file

---
+++ parse.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://fragment.com/numbers?sort=price_asc&filter=sale'
+
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+print(soup)
+
+# with open('parse.txt', 'w+') as file:
+#     file.write(str(soup))
