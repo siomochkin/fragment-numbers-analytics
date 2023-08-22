463146828ae41a58d29b0073a61e4c45625b39fe
DenysReryt <denysreryt28@gmail.com>
2023-08-23 01:03:48 +0300

    Parsing required info for database

--- parse.py
+++ parse.py
@@ -1,12 +1,51 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 
-url = 'https://fragment.com/numbers?sort=price_asc&filter=sale'
+url1 = 'https://fragment.com/numbers?sort=price_asc&filter=sale'
+url2 = 'https://coinmarketcap.com/en/currencies/toncoin/'
 
-r = requests.get(url, timeout=60)
-soup = BeautifulSoup(r.text, 'html.parser')
+    
+while True:
+    # For fragment.com
+    fragment = requests.get(url1, timeout=1)
+    soup1 = BeautifulSoup(fragment.text, 'html.parser')
+    
+    # For coinmarketcap.com
+    coinmarket = requests.get(url2, timeout=1)
+    soup2 = BeautifulSoup(coinmarket.text, 'html.parser')
+    
+    # Find first phone number
+    phone_find = str(soup1)[str(soup1).find('tm-value'):]
+    phone = phone_find[phone_find.find('>')+1:phone_find.find('<')]
 
-print(soup)
+    # Find number status
+    status_find = str(soup1)[str(soup1).find('status-avail'):]
+    status = status_find[status_find.find('>')+1:status_find.find('<')]
 
-# with open('parse.txt', 'w+') as file:
-#     file.write(str(soup))
+    # Find price in TON
+    ton_find = str(soup1)[str(soup1).find('icon-ton'):]
+    ton = ton_find[ton_find.find('>')+1:ton_find.find('<')]
+
+    # Get price($)
+    price_find = str(soup2)[str(soup2).find('The live Toncoin price today'):]
+    price = float(price_find[price_find.find('$')+1:price_find.find('USD')]) * float(ton)
+
+    # Find remaining time
+    data_find = str(soup1)[str(soup1).find('"text" datetime='):]
+    data = data_find[data_find.find('>')+1:data_find.find('<')]
+    param1 = ['days', 'day', 'hours', 'hour']
+    param2 = ['minutes', 'minute', ' ']
+    for i in param2:
+        data = data.replace(i, '')
+        for i in param1:
+            data = data.replace(i, ':', 1)
+        
+            
+
+    
+    print(phone, status, ton, data, price)
+    time.sleep(5)
+    
+# with open('coinmarket.txt', 'w+') as file:
+#     file.write(str(soup2))
