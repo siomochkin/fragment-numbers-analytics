8b1da67f71b44e78c2e885b93a4a5d6acedd1baa
DenysReryt <denysreryt28@gmail.com>
2023-08-23 14:38:10 +0300

    Write data in database

--- database.py
+++ database.py
@@ -9,27 +9,21 @@ try:
         password=s.DB_PASSWORD, 
         port=s.DB_PORT)
     
-    connection.autocommit = True
-    
     # Create table numbers
     with connection.cursor() as cursor:
         cursor.execute(
             """CREATE TABLE numbers(
                 id SERIAL PRIMARY KEY,
-                data TIMESTAMPTZ NOT NULL,
+                date TIMESTAMPTZ NOT NULL,
                 price NUMERIC(8,2) NOT NULL,
-                ton  NUMERIC(8,5) NOT NULL,
+                ton  NUMERIC(8,3) NOT NULL,
                 phone VARCHAR(50) NOT NULL, 
                 status VARCHAR(50) NOT NULL,
                 remaining_time VARCHAR(50) NOT NULL
                 );"""
         )
-        
+        # connection.commit()
         print(f"Server version: {cursor.fetchone()}")
     
 except (Exception, psycopg2.Error) as error:
-    print("Error while connecting to PostgreSQL::", error)
-finally:
-    if connection:
-        connection.close()
-        print('[INFO] PostgreSQL connection closed')
+    print("Error while connecting to PostgreSQL:", error)
--- parse.py
+++ parse.py
@@ -1,51 +1,60 @@
 import requests
 import time
+from datetime import datetime
 from bs4 import BeautifulSoup
+from database import connection
 
 url1 = 'https://fragment.com/numbers?sort=price_asc&filter=sale'
 url2 = 'https://coinmarketcap.com/en/currencies/toncoin/'
 
-    
-while True:
-    # For fragment.com
-    fragment = requests.get(url1, timeout=1)
-    soup1 = BeautifulSoup(fragment.text, 'html.parser')
-    
-    # For coinmarketcap.com
-    coinmarket = requests.get(url2, timeout=1)
-    soup2 = BeautifulSoup(coinmarket.text, 'html.parser')
-    
-    # Find first phone number
-    phone_find = str(soup1)[str(soup1).find('tm-value'):]
-    phone = phone_find[phone_find.find('>')+1:phone_find.find('<')]
-
-    # Find number status
-    status_find = str(soup1)[str(soup1).find('status-avail'):]
-    status = status_find[status_find.find('>')+1:status_find.find('<')]
-
-    # Find price in TON
-    ton_find = str(soup1)[str(soup1).find('icon-ton'):]
-    ton = ton_find[ton_find.find('>')+1:ton_find.find('<')]
-
-    # Get price($)
-    price_find = str(soup2)[str(soup2).find('The live Toncoin price today'):]
-    price = float(price_find[price_find.find('$')+1:price_find.find('USD')]) * float(ton)
-
-    # Find remaining time
-    data_find = str(soup1)[str(soup1).find('"text" datetime='):]
-    data = data_find[data_find.find('>')+1:data_find.find('<')]
-    param1 = ['days', 'day', 'hours', 'hour']
-    param2 = ['minutes', 'minute', ' ']
-    for i in param2:
-        data = data.replace(i, '')
-        for i in param1:
-            data = data.replace(i, ':', 1)
+try:
+    while True:   
+        # For fragment.com
+        fragment = requests.get(url1)
+        soup1 = BeautifulSoup(fragment.text, 'html.parser')
         
-            
+        # For coinmarketcap.com
+        coinmarket = requests.get(url2)
+        soup2 = BeautifulSoup(coinmarket.text, 'html.parser')
+        
+        # Find first phone number
+        phone_find = str(soup1)[str(soup1).find('tm-value'):]
+        phone = str(phone_find[phone_find.find('>')+1:phone_find.find('<')])
 
-    
-    print(phone, status, ton, data, price)
-    time.sleep(5)
-    
-# with open('coinmarket.txt', 'w+') as file:
-#     file.write(str(soup2))
+        # Find number status
+        status_find = str(soup1)[str(soup1).find('status-avail'):]
+        status = status_find[status_find.find('>')+1:status_find.find('<')]
+
+        # Find price in TON
+        ton_find = str(soup1)[str(soup1).find('icon-ton'):]
+        ton = ton_find[ton_find.find('>')+1:ton_find.find('<')]
+
+        # Get price($)
+        price_find = str(soup2)[str(soup2).find('The live Toncoin price today'):]
+        price = float(price_find[price_find.find('$')+1:price_find.find('USD')]) * float(ton)
+
+        # Find remaining time
+        data_find = str(soup1)[str(soup1).find('"text" datetime='):]
+        data = data_find[data_find.find('>')+1:data_find.find('<')]
+        param1 = ['days', 'day', 'hours', 'hour']
+        param2 = ['minutes', 'minute', ' ']
+        for i in param2:
+            data = data.replace(i, '')
+            for i in param1:
+                data = data.replace(i, ':')
+                
+        # Insert data into DB
+        try:
+            with connection.cursor() as cursor:
+                query = """INSERT INTO numbers (date, price, ton, phone, status, remaining_time) VALUES(%s, %s, %s, %s, %s, %s);"""
+                values = (datetime.now(), price, ton, phone, status, data)
+                cursor.execute(query, values)
+                connection.commit()
+                time.sleep(57)
+        except Exception as e:
+            connection.rollback()
+            print('Error: ', e)
+        
+except Exception as e:
+    connection.close()
+    print('Error:', e)
