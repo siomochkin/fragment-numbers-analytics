6765c11f2334e0072ae19ca4606aa699d0b36762
DenysReryt <denysreryt28@gmail.com>
2023-09-05 16:55:13 +0300

    Add try-except block

--- buy.py
+++ buy.py
@@ -6,31 +6,38 @@ from datetime import datetime
 def main():
     url_fragment = 'https://fragment.com/numbers?sort=price_asc&filter=sale'
     url_coinmarket = 'https://coinmarketcap.com/en/currencies/toncoin/'
-
+    
     while True:   
-        # For fragment.com
-        fragment = get_parsed_data(url_fragment)
+        try:
+            # For fragment.com
+            fragment = get_parsed_data(url_fragment)
+                
+            # For coinmarketcap.com
+            coinmarket = get_parsed_data(url_coinmarket)
+                
+            phone = get_phone_number(fragment)
+            status = get_number_status(fragment)
+            ton = get_price_in_ton(fragment)
+
+            price_data = str(coinmarket)[str(coinmarket).find('The live Toncoin price today'):]
+            price = float(price_data[price_data.find('$')+1:price_data.find('USD')]) * float(ton)
+
+            data = get_remaining_time(fragment)
             
-        # For coinmarketcap.com
-        coinmarket = get_parsed_data(url_coinmarket)
+            values = (str(datetime.now()), str(data), round(price, 2), ton, str(phone), str(status))
+
+            with open('buy-numbers.csv', 'a+') as file:
+                csv_writer = csv.writer(file)
+                csv_writer.writerow(values)
+                print(values)
             
-        phone = get_phone_number(fragment)
-        status = get_number_status(fragment)
-        ton = get_price_in_ton(fragment)
-
-        price_data = str(coinmarket)[str(coinmarket).find('The live Toncoin price today'):]
-        price = float(price_data[price_data.find('$')+1:price_data.find('USD')]) * float(ton)
-
-        data = get_remaining_time(fragment)
+            time.sleep(5)
         
-        values = (str(datetime.now()), str(data), str(round(price, 2)), str(ton), str(phone), str(status))
-
-        with open('../buy-numbers.csv', 'a+') as file:
-            csv_writer = csv.writer(file)
-            csv_writer.writerow(values)
-            print(values)
-        
-        time.sleep(55)
+        except Exception as e:
+            print(e)
+            
+        finally:
+            continue
        
 if __name__ == '__main__':
     main()
--- sold/sold.py
+++ sold/sold.py
@@ -7,31 +7,38 @@ def main():
     url_fragment = 'https://fragment.com/numbers?sort=ending&filter=sold'
     url_coinmarket = 'https://coinmarketcap.com/en/currencies/toncoin/'
 
-    while True:   
-        # For fragment.com
-        fragment = get_parsed_data(url_fragment)
+    while True: 
+        try:  
+            # For fragment.com
+            fragment = get_parsed_data(url_fragment)
+                
+            # For coinmarketcap.com
+            coinmarket = get_parsed_data(url_coinmarket)
+                
+            phone = get_phone_number(fragment)
+            status = get_number_status(fragment)
+            ton = get_price_in_ton(fragment)
+
+            price_data = str(coinmarket)[str(coinmarket).find('The live Toncoin price today'):]
+            price = float(price_data[price_data.find('$')+1:price_data.find('USD')]) * float(ton)
+
+            sold_time = get_sold_time(fragment)
+            formatted_sold_time = convert_to_datetime_and_format(sold_time)
             
-        # For coinmarketcap.com
-        coinmarket = get_parsed_data(url_coinmarket)
+            values = (formatted_sold_time, round(price, 2), ton, phone, status)
+
+            with open('sold/sold-numbers.csv', 'a+') as file:
+                csv_writer = csv.writer(file)
+                csv_writer.writerow(values)
+                print(values)
             
-        phone = get_phone_number(fragment)
-        status = get_number_status(fragment)
-        ton = get_price_in_ton(fragment)
-
-        price_data = str(coinmarket)[str(coinmarket).find('The live Toncoin price today'):]
-        price = float(price_data[price_data.find('$')+1:price_data.find('USD')]) * float(ton)
-
-        sold_time = get_sold_time(fragment)
-        formatted_sold_time = convert_to_datetime_and_format(sold_time)
+            time.sleep(55)
+            
+        except Exception as e:
+            print(e)
+            
+        finally:
+            continue
         
-        values = (formatted_sold_time, str(round(price, 2)), str(ton), str(phone), str(status))
-
-        with open('sold-numbers.csv', 'a+') as file:
-            csv_writer = csv.writer(file)
-            csv_writer.writerow(values)
-            print(values)
-        
-        time.sleep(55)
-       
 if __name__ == '__main__':
     main()
